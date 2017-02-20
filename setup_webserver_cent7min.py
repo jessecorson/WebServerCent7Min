aaa56afb97ad05b487262e5db9ffd4a247206e32
Jesse Corson <jcorson@shoretel.com>
2017-02-20 09:57:40 -0500

    uploading python script

---
+++ setup_webserver_cent7min.py
@@ -0,0 +1,25 @@
+#!/bin/python
+
+from subprocess import call
+
+print('starting setup')
+
+apps = ('php', 'vim', 'net-tools', 'telnet')
+sysops = ('enable', 'start', 'status')
+reload = ('reload', 'status')
+svcs = ('--add-service=http','--add-service=https')
+
+
+for app in apps:
+  call(['yum', 'install', app, '-y'])
+
+for op in sysops:
+  call(['systemctl', op, 'httpd'])
+
+for svc in svcs:
+  call(['firewall-cmd', '--zone=public', '--permanent', svc])
+
+for relo in reload:
+  call(['systemctl', relo, 'firewalld'])
+
+print('setup complete')
